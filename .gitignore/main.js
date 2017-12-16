7ce721a1ff7405c6112b8144ff52354f7839cf37
Xariox <34604033+Xariox@users.noreply.github.com>
2017-12-16 22:41:34 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -19,9 +19,11 @@ bot.on('message', message => {
 
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
-            .setColor('#CC00DB')
-            .addField("Général", "    - /help : Affiche les commandes du bot !", true)
-            .addField("Interaction", "    - /ping : Le bot répond pong !")
+            .setColor('#B200FE')
+            .setTitle('Liste des commandes du bot !')
+            .addField("/help", "Affiche les commandes du bot !", true)
+            .addField("/ping", "Le bot répond pong !", true)
+            .addField("/stats", "Affiche votre profil discord !", true)
             .setFooter("BattleSky by Xari0x", "https://cdn.discordapp.com/avatars/282147518958272512/4746c6bc75b7de27df5990a4fb70ec1c.png")
             .setThumbnail("https://cdn.discordapp.com/avatars/354986240476446721/954bbdcc03988600ad8fae6226c3726a.png")
         message.channel.sendEmbed(help_embed);
@@ -30,7 +32,7 @@ bot.on('message', message => {
 
     if (message.content === prefix + "ping"){
         var ping_embed = new Discord.RichEmbed()
-            .setColor('#07DB00')
+            .setColor('#B200FE')
             .addField("Ping", "Pong !")
             .setFooter("BattleSky by Xari0x", "https://cdn.discordapp.com/avatars/282147518958272512/4746c6bc75b7de27df5990a4fb70ec1c.png")
         message.channel.sendEmbed(ping_embed);
@@ -38,11 +40,17 @@ bot.on('message', message => {
     }
 
     if (message.content === prefix + "stats"){
+        var userCreateDate = message.author.createdAt.toString().split(' ');
+        var userPing = message.author.client.ping
         var stats_embed = new Discord.RichEmbed()
-            .addField("Utilisateur", "${message.author.username}", true)
-            .addField("UserID", author)
-
-        message.author.send({embed: stats_embed});
+            .setColor('#B200FE')
+            .addField("Utilisateur", message.author, true)
+            .addField("UserID", message.author.id, true)
+            .addField("Date de création", userCreateDate[1] + ' ' + userCreateDate[2] + ', ' + userCreateDate[3], true)
+            .addField("Ping", userPing, true)
+            .setThumbnail(message.author.avatarURL)
+            .setFooter("BattleSky by Xari0x", "https://cdn.discordapp.com/avatars/282147518958272512/4746c6bc75b7de27df5990a4fb70ec1c.png")
+        message.channel.sendEmbed(stats_embed);
     }
 
 });
