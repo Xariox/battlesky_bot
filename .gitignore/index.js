47c20a49471ef40bc8bacb9d1743a333580e4caf
Xariox <34604033+Xariox@users.noreply.github.com>
2017-12-16 21:06:27 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+var prefix = ("/");
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: 'BattleSky [/help]', type: 0} });
+    console.log("Bot Ready !");
+});
+
+bot.login('MzU0OTg2MjQwNDc2NDQ2NzIx.DRbmHg.HEuanQT8kjVE8pQbuUL7MeHDbwI');
+
+bot.on("guildMemberAdd", member => {
+    let role = member.guild.roles.find("name", "☃️ Visiteur ☃️");
+    member.addRole(role)
+})
+
+bot.on('message', message => {
+
+    if (message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed()
+            .setColor('#CC00DB')
+            .addField("Général", "    - /help : Affiche les commandes du bot !")
+            .addField("Interaction", "    - /ping : Le bot répond pong !")
+            .setFooter("BattleSky by Xari0x", "https://cdn.discordapp.com/avatars/282147518958272512/4746c6bc75b7de27df5990a4fb70ec1c.png")
+            .setThumbnail("https://cdn.discordapp.com/avatars/354986240476446721/954bbdcc03988600ad8fae6226c3726a.png")
+        message.channel.sendEmbed(help_embed);
+        console.log("Commande /help demandée !");
+    }
+
+    if (message.content === prefix + "ping"){
+        var help_embed = new Discord.RichEmbed()
+            .setColor('#07DB00')
+            .addField("Ping", "Pong !")
+            .setFooter("BattleSky by Xari0x", "https://cdn.discordapp.com/avatars/282147518958272512/4746c6bc75b7de27df5990a4fb70ec1c.png")
+        message.channel.sendEmbed(help_embed);
+        console.log('Commande /ping demandée ! Pong !');
+    }
+
+});
